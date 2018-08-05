0e0c35d36810ba70ac829833560afa1c54e105cf
Test <Test@gmail.com>
2018-08-05 04:35:23 -0700

    My test thrid commit

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from myapp import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+	user = {'username': 'Migel'}
+	return render_template('index.html', 
+		title='Home', 
+		user=user)
+	#return "Hello, world!"
